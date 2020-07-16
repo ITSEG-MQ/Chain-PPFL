f12241d6b36e217ae5501dfd611c923f0b071927
Macquarie IoT SE Group <55269043+ITSEG-MQ@users.noreply.github.com>
2020-07-16 16:56:04 +1000

    Add files via upload

---
+++ models/Fed.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+import copy
+import torch
+from torch import nn
+
+
+def FedAvg(w):
+    w_avg = copy.deepcopy(w[0])
+    for k in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[k] += w[i][k]
+        w_avg[k] = torch.div(w_avg[k], len(w))
+    return w_avg
+
+def FedAvg_serial(w):
+    w_avg = copy.deepcopy(w[-1])
+    for k in w_avg.keys():
+        w_avg[k] = torch.div(w_avg[k]-w[0][k], len(w)-1)
+    return w_avg
